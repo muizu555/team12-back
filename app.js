079d1360e7547d864b25d5afd944ccd18b049b31
muizu <muizu0920555@protonmail.com>
2023-06-24 23:30:29 +0900

    change

--- app.js
+++ app.js
@@ -2,9 +2,24 @@ const express = require("express");
 const app = express();
 
 
-app.use(express.json());
+app.use(express.json());//res.json(data)などを使う時のためのもの
 
-const PORT = 3000;
+const PORT = 80;
+
+app.use(express.static("public"));
+
+
+
+
+
+app.get("/hello/:name", (req, res) => {
+    console.log(req.params.name);//ターミナルに表示させる
+    res.send("OK\n");
+});
+
+
+
+
 
 app.listen(PORT, () =>{
     console.log("server running");
