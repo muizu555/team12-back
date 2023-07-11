ad9f6f98f3a830d257dfd905e639f59443992771
muizu <muizu0920555@protonmail.com>
2023-07-11 14:31:02 +0900

    apiを叩いている

--- routes/user.js
+++ routes/user.js
@@ -19,6 +19,8 @@ router.get("",async(req, res) => {//ここのエンドポイントどうする�
 
 
 
+
+
 setInterval(myCallback, 1000*60*20, Playlist, User, 20);
 
 ///結局for文で回す必要がある
@@ -26,54 +28,58 @@ setInterval(myCallback, 1000*60*20, Playlist, User, 20);
 async function myCallback (Playlist, User, interval){//できる
     await Playlist.deleteMany({});//ここで今までのplaylistを全て消去している
     //ここで登録している全てのユーザーに対してapiを叩く
-    const user  = await User.findById(req.params.id);
-    console.log(user.PlaylistId);
-    const data = await fetch("googleエンドポイント");//個人のデータはとってこれている
-    for(let i =0; i<data.items.length;i++){
-        const publishedAt = data.items[i].snippet.publishedAt;
-        const videoId = data.items[i].snippet.resourceId.videoId;
-        const find = await Video.exists({ videoId: videoId });//
-        if(find === null){
-            continue;
-        }
-        const data2 = await fetch("googleのエンドポイント");
-        const duration = data2.items.contentDetails.duration;
-        const Video = new video({ publishedAt: publishedAt, duration: duration});
-        const newVideo = await Video.save();
-        const find2 = await Playlist.exists({PlaylistId: user.PlaylistId})
-        if(find2 === null){
-            const newPlayList = new Playlist({PlaylistId: user.PlaylistId});
-            const finPlayList = await newPlayList.save();
-            await finPlayList.updateOne({
+    const user  = await User.find({});//全ユーザーを取ってくる
+    for(let i = 0 ;i < user.length; i++){//ここで一人ずつ叩いている
+        const userId = user[i]._id;
+        //ここでuserIdを持った状態で叩く、多分ここはtokenになるはずである
+        const data = await fetch("googleエンドポイント");//個人のデータはとってこれている
+        for(let i =0; i<data.items.length;i++){
+            const publishedAt = data.items[i].snippet.publishedAt;
+            const videoId = data.items[i].snippet.resourceId.videoId;
+            const find = await Video.exists({ videoId: videoId });//
+            if(find === null){
+                continue;
+            }
+            const data2 = await fetch("googleのエンドポイント");
+            const duration = data2.items.contentDetails.duration;
+            const Video = new video({ publishedAt: publishedAt, duration: duration});
+            const newVideo = await Video.save();
+            const find2 = await Playlist.exists({PlaylistId: user.PlaylistId})
+            if(find2 === null){
+                const newPlayList = new Playlist({PlaylistId: user.PlaylistId});
+                const finPlayList = await newPlayList.save();
+                await finPlayList.updateOne({
+                    $push: {
+                        items: newVideo._id,//投稿のobjectIdを挿入している。
+                    },
+                });//ここでfinPlayListが完成形
+            }
+            await Playlist.updateOne({
                 $push: {
-                    items: newVideo._id,//投稿のobjectIdを挿入している。
+                    items: newVideo._id,
                 },
-            });//ここでfinPlayListが完成形
+            });
         }
-        await Playlist.updateOne({
-            $push: {
-                items: newVideo._id,
-            },
-        });
-    
     }
-
-
-    
-
-
-
-
-
-
-
-
-
-
-
-
-    
 }
+    
+
+
+    
+
+
+
+
+
+
+
+
+
+
+
+
+    
+
 
 
 
