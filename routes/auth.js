2dfeb0ef24d2656820b2f8f156b197fa58b162c9
muizu <muizu0920555@protonmail.com>
2023-07-13 13:33:02 +0900

    auth

--- routes/auth.js
+++ routes/auth.js
@@ -2,37 +2,39 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
 const Playlist = require("../models/Playlist");
+const { google } = require('googleapis');
 //const bcrypt = require("bcrypt");
 
+// Google OAuth2のクライアント情報
+const clientId = 'YOUR_CLIENT_ID';
+const clientSecret = 'YOUR_CLIENT_SECRET';
+const redirectUri = 'YOUR_REDIRECT_URI';
 
-router.post("/googleauth", async(req,res) => {
-    try {
-        const { } = req.body;
+router.post("/googleauth", async (req, res) => {
+  try {
+    const code = req.body;
+    console.log(code);
 
-        //code accessTokenとrefleshTokenに変えるコードを書く
+    // アクセストークンとリフレッシュトークンを取得
+    const { tokens } = await google.auth.getToken({
+      code,
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: redirectUri,
+    });
 
+    const accessToken = tokens.access_token;
+    const refreshToken = tokens.refresh_token;
 
+    // 取得したトークンを利用して何かを実行するなどの処理を追加する
 
+    // レスポンスとしてアクセストークンとリフレッシュトークンを返す
+    res.json({ accessToken, refreshToken });
 
-
-
-        //
-        console.log(username);
-        console.log(playlistId);
-
-        
-        const newUser = await new User({
-            username: username,
-            playlistId: playlistId,
-        });
-        const user = await newUser.save();
-        const newPlayList = await new Playlist({playListId: playlistId});
-        const playlist = await newPlayList.save();
-        //req.session.user_id = user._id;  //一応sessionに保存している 後で実装
-        return res.status(200).json({user,playlist});
-    } catch (err) {
-        return res.status(500).json(err.message);//500はサーバー関連のerr
-    }
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
 });
 
 
