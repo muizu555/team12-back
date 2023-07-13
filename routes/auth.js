b6208f9e7fb93641e9912e05eca19985af1d0ae4
muizu <muizu0920555@protonmail.com>
2023-07-13 15:34:00 +0900

    mistake

--- routes/auth.js
+++ routes/auth.js
@@ -12,42 +12,6 @@ const clientId = process.env.YOUR_CLIENT_ID;
 const clientSecret = process.env.YOUR_CLIENT_SECRET;
 const redirectUri = process.env.YOUR_REDIRECT_URI;
 
-router.post("/googleauth", async (req, res) => {
-  try {
-    const code = req.body;
-    console.log(code);
-
-    // アクセストークンとリフレッシュトークンを取得
-    const { tokens } = await google.auth.getToken({
-      code,
-      client_id: clientId,
-      client_secret: clientSecret,
-      redirect_uri: redirectUri,
-    });
-
-    const accessToken = tokens.access_token;
-    const refreshToken = tokens.refresh_token;
-    console.log(accessToken);
-
-    // 取得したトークンを利用して何かを実行するなどの処理を追加する
-    const auth = new Auth({
-        userId = req.session.
-        accessToken: accessToken,
-        refreshToken: refreshToken,
-    })
-    
-
-    // レスポンスとしてアクセストークンとリフレッシュトークンを返す
-    res.json({ accessToken, refreshToken });
-
-  } catch (error) {
-    console.error(error);
-    res.status(500).json({ error: 'Internal Server Error' });
-  }
-});
-
-
-
 
 
 
@@ -64,6 +28,7 @@ router.post("/register", async(req,res) => {
             playlistId: playlistId,
         });
         const user = await newUser.save();
+        req.session.user_id = user._id;  ///ここでユーザーIDをセッションに保存している
         const newPlayList = await new Playlist({playListId: playlistId});
         const playlist = await newPlayList.save();
         //req.session.user_id = user._id;  //一応sessionに保存している 後で実装
@@ -72,6 +37,48 @@ router.post("/register", async(req,res) => {
         return res.status(500).json(err.message);//500はサーバー関連のerr
     }
 });
+
+
+router.post("/googleauth", async (req, res) => {
+  try {
+    const code = req.body.code;
+    console.log(code);
+
+    // アクセストークンとリフレッシュトークンを取得
+    const { tokens } = await google.auth.getAccessToken()
+    
+    ({
+      code,
+      client_id: clientId,
+      client_secret: clientSecret,
+      redirect_uri: redirectUri,
+    });
+
+    const accessToken = tokens.access_token;
+    const refreshToken = tokens.refresh_token;
+    console.log(accessToken);
+
+    // 取得したトークンを利用して何かを実行するなどの処理を追加する
+    const newAuth = new Auth({
+        userId: req.session.user_id,
+        accessToken: accessToken,
+        refreshToken: refreshToken,
+    });
+
+    const auth = await newAuth.save();
+
+    // レスポンスとしてアクセストークンとリフレッシュトークンを返す
+    res.status(200).json(auth);
+
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({ error: 'Internal Server Error' });
+  }
+});
+
+
+
+
 
 
 
